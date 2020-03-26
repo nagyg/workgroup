cb0ee3b491404319b26d287c972260dd97ccb6c2
nagyg <gergo@melon.hu>
2020-03-26 15:02:32 +0100

    move url repo

--- bash/source/main.sh
+++ bash/source/main.sh
@@ -22,7 +22,7 @@ pathshow () { printenv PATH | sed 's|:|\n|g'; }
 #------------------------------------------////
 # Bin:
 #------------------------------------------////
-pathadd "${WGPATH}/bin/7za"
+pathadd "${WGPATH}/bin/wget-1.20.3-win32"
 
 #------------------------------------------////
 # Bash Finction & Alias:
@@ -64,6 +64,28 @@ tree () {
 
 reload() {
 	source ~/.bash_profile
+}
+
+extract() {
+ if [ -f "$1" ] ; then
+  case "$1" in
+  *.tar.bz2) tar xjf "$1" ;;
+  *.tar.gz) tar xzf "$1" ;;
+  *.tar.Z) tar xzf "$1" ;;
+  *.bz2) bunzip2 "$1" ;;
+  *.rar) unrar x "$1" ;;
+  *.gz) gunzip "$1" ;;
+  *.jar) unzip "$1" ;;
+  *.tar) tar xf "$1" ;;
+  *.tbz2) tar xjf "$1" ;;
+  *.tgz) tar xzf "$1" ;;
+  *.zip) unzip "$1" ;;
+  *.Z) uncompress "$1" ;;
+  *) echo "'$1' cannot be extracted." ;;
+ esac
+ else
+  echo "'$1' is not a file."
+ fi
 }
 
 # some more ls aliases
--- bash/source/repo.sh
+++ bash/source/repo.sh
@@ -2,39 +2,42 @@
 #--------------------------------------------------------------------------------------------------////
 # URL repo:
 #--------------------------------------------------------------------------------------------------////
+
+export url="http://nagyg.ddns.net:17600/workgroup/win"
+
 repo.archive() {
-local d
+local dir arg
 local in=`realpath .`
 local archive_dir="${WGPATH}/"
-
-function 7zazip {
-7za a -tzip "${WGPATH}/tmp/${archive}" -x@.repoignore
-	if [ $? -eq 0 ]; then
-		echo -e "[${green}${WGPATH}/tmp/${archive}.zip${nc}]\n"
-	else
-		echo -e "[${red}${WGPATH}/tmp/${archive}.zip${nc}]\n"
-	fi
-}
+local ext=tar.gz
 
 if [ "$#" == 0 ]; then
-	for d in "${archive_dir}"* ; do
-		local archive=${d##${archive_dir}}
-		if [ -f "$d/.repoignore" ]; then
-			builtin cd "$d"
-			7zazip
-		fi
+	for dir in "${archive_dir}"* ; do
+		local archive=${dir##${archive_dir}}
+		tar.tmp
 	done
 else
-	for d in "${@}"; do
-		local archive=$d
-		if [ -f "${WGPATH}/$d/.repoignore" ]; then
-			builtin cd "${WGPATH}/$d"
-			7zazip
-		else
-			echo -e "[${red}${WGPATH}/$d${nc}]	>> .repoignore file not found"
-		fi
+	for arg in "${@}"; do
+		local archive=$arg
+		local dir=${WGPATH}/$arg
+		tar.tmp
 	done
+fi
 builtin cd "$in"
+}
+
+function tar.tmp {
+mkdir -p ${WGPATH}/tmp
+if [ -f "$dir/.repoignore" ]; then
+	builtin cd "$dir"
+	tar -zcvf "${WGPATH}/tmp/${archive}.tar.gz" -X "$dir/.repoignore" .
+	if [ $? -eq 0 ]; then
+		echo -e "[${green}${WGPATH}/tmp/${archive}.${ext}${nc}]\n"
+	else
+		echo -e "[${red}${WGPATH}/tmp/${archive}.${ext}${nc}]\n"
+	fi
+else
+	echo -e "[${dir}]	>> .repoignore file not found"
 fi
 }
 
@@ -43,34 +46,25 @@ if [ "$#" == 0 ]; then
 	echo "bash: [$#]: illegal number of parameters"
 else
 	local i
-	local in=`realpath .`
-	local ext=zip
-
-
-	local url0="http://nagyg.ddns.net:17600/workgroup/win"
+	local ext=tar.gz
 
 	for i in "${@}"; do
-		curl -sSf $url0/${i}.${ext} > /dev/null
-		if [ $? -eq 23 ]; then
+		wget -q --spider $url/${i}.${ext} > /dev/null
+		if [ $? -eq 0 ]; then
+
 			local path=`realpath "${WGPATH}/${i}"`
-			if [ -d "$path" ]; then
-				builtin cd "$path"
-			else
-				mkdir "$path"
-				builtin cd "$path"
-			fi
-			curl $url0/${i}.${ext} --output ./${i}.${ext}
-			7za x ${i}.${ext} -r -aoa && rm ${i}.${ext}
+
+			wget $url/${i}.${ext} -P "${path}"
+			tar -xf ${path}/${i}.${ext} -C ${path} && rm ${path}/${i}.${ext}
 				if [ $? -eq 0 ]; then
-					echo -e "[${green}$url0/${i}.${ext}${nc}]	${green}>> ${i}${nc}\n"
+					echo -e "[${green}$url/${i}.${ext}${nc}]	${green}>> ${i}${nc}\n"
 				else
-					echo -e "[${red}$url0/${i}.${ext}${nc}]\n"
+					echo -e "[${red}$url/${i}.${ext}${nc}]\n"
 				fi
 		else
-			echo -e "[${red}$url0/${i}.${ext}${nc}]\n"
+			echo -e "[${red}$url/${i}.${ext}${nc}]\n"
 		fi
 	done
-	builtin cd "$in"
 fi
 }
 
