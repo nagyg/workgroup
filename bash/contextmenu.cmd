2ad7f4f75503e397c2302a431197fe5e3a8f28d9
nagyg <gergo@melon.hu>
2020-05-19 01:03:20 +0200

    mpro contextmenu

--- bash/contextmenu.cmd
+++ bash/contextmenu.cmd
@@ -17,15 +17,15 @@ echo ===========================================================
 echo                        REGEDIT
 echo ===========================================================
 echo HKEY_CLASSES_ROOT\Directory\shell\SafeDir\command
-reg add "HKEY_CLASSES_ROOT\Directory\shell\SafeDir\command" /t REG_SZ /d "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -File %WGPATH%\mpro\context_menu\SafeDir.ps1 \"%%L\""
+reg add "HKEY_CLASSES_ROOT\Directory\shell\SafeDir\command" /t REG_SZ /d "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -File %WGPATH%\mpro\contextmenu\SafeDir.ps1 \"%%L\""
 echo(
 
 echo HKEY_LOCAL_MACHINE\SOFTWARE\Classes\*\shell\Safe\Command
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\*\shell\Safe\Command" /t REG_SZ /d "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -File %WGPATH%\mpro\context_menu\Safe.ps1 \"%%L\""
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\*\shell\Safe\Command" /t REG_SZ /d "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -File %WGPATH%\mpro\contextmenu\Safe.ps1 \"%%L\""
 echo(
 
 echo HKEY_CLASSES_ROOT\Directory\shell\Version\command
-reg add "HKEY_CLASSES_ROOT\Directory\shell\Version\command" /t REG_SZ /d "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -File %WGPATH%\mpro\context_menu\Version.ps1 \"%%L\""
+reg add "HKEY_CLASSES_ROOT\Directory\shell\Version\command" /t REG_SZ /d "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -File %WGPATH%\mpro\contextmenu\Version.ps1 \"%%L\""
 echo(
 echo ===========================================================
 goto exit
