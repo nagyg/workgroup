f393363a5564d4ee8b7fba79373208720ea64345
nagyg <gergo@melon.hu>
2020-12-18 13:02:05 +0100

    12/18/20 13:02:05 gergo

--- setup/source/opencolorio.sh
+++ setup/source/opencolorio.sh
@@ -2,9 +2,4 @@
 #--------------------------------------------------------------------------------------------------////
 # OpenColorIO:
 #--------------------------------------------------------------------------------------------------////
-# FUCK RSR VIEW UNC
-if [ -d "/c/opencolorio" ]; then
-    export OCIO="$(cygpath -w "/c/opencolorio/aces_1.0.3/config.ocio")"
-else
-    export OCIO="$(cygpath -w "${WGPATH}/opencolorio/aces_1.0.3/config.ocio")"
-fi
+export OCIO="$(cygpath -w "${WGPATH}/opencolorio/aces_1.0.3/config.ocio")"
