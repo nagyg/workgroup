568a48bffcde74377d683bf78566937208b5d21a
nagyg <gergo@melon.hu>
2021-04-19 10:44:06 +0200

    04/19/21 10:44:06 gergo

--- setup/source/opencolorio.sh
+++ setup/source/opencolorio.sh
@@ -3,3 +3,10 @@
 # OpenColorIO:
 #--------------------------------------------------------------------------------------------------////
 export OCIO="$(cygpath -w "${WGPATH}/opencolorio/aces_1.0.3/config.ocio")"
+
+ocio.converter () {
+    local in=`realpath .`
+    builtin cd "${WGPATH}/opencolorio/OCIOConverter"
+    "${WGPATH}/opencolorio/OCIOConverter/OCIOConverter.exe" &
+    builtin cd "$in"
+}
